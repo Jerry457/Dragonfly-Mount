69c0b3c03d70a256984c668239d614d0d2e0014f
荧之空 <2380749795@qq.com>
2026-03-01 03:55:10 +0800

    Disable spell_book with controller

--- postinit/components/playercontroller.lua
+++ postinit/components/playercontroller.lua
@@ -6,6 +6,10 @@ local Playercontroller = require("components/playercontroller")
 -- TryAOETargeting 开/关施法轮盘
 local _TryAOETargeting = Playercontroller.TryAOETargeting
 function Playercontroller:TryAOETargeting(...)
+    if TheInput:ControllerAttached() then
+        return _TryAOETargeting(self, ...)
+    end
+
     local rider = self.inst.replica.rider
     local mount = rider and rider:GetMount()
     if mount and mount:HasTag("dragonfly_mount") and mount.components.spellbook and mount.components.spellbook:CanBeUsedBy(self.inst) then
@@ -16,9 +20,9 @@ function Playercontroller:TryAOETargeting(...)
             buffaction = BufferedAction(self.inst, nil, ACTIONS.USESPELLBOOK, mount)
         end
         buffaction.action.pre_action_cb(buffaction)
-        -- 这里只要打开轮盘界面即可
         return true
     end
+
     return _TryAOETargeting(self, ...)
 end
 
